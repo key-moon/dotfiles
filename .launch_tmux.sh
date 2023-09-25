69421a52c86a92c95f4c5cc38e2c5a0c33b8ae02
key-moon <kymn0116@gmail.com>
2023-09-25 15:48:59 +0900

    fast launch

---
+++ .launch_tmux.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if tmux ls; then
+ # server exists
+  SESSION=$(echo $(tmux ls -F '#S' -f '#{==:#{session_attached},0}') $(tmux display -p '#{?#{session_attached},,#S}') | awk -F ' ' '{print $NF}')
+  if [ -z "$SESSION" ] ; then
+    tmux
+  else
+    tmux attach-session -t $SESSION
+  fi
+else
+ # server must spawned from zsh
+  echo "launching..."
+  /bin/zsh -i -c tmux
+fi
